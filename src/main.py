d65b3fde1ae5bec562354cabcebe9de6cb75d077
EleazarMartin <135169782+EleazarMartin@users.noreply.github.com>
2025-06-12 16:33:51 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from src.train import load_model_from_config
