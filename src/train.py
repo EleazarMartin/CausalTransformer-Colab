fd22206d0c12f861c6c34453e8e34a0d13c9381e
EleazarMartin <135169782+EleazarMartin@users.noreply.github.com>
2025-06-12 17:24:34 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -3,5 +3,10 @@ from src.data.cancer_sim.dataset import SyntheticDatasetCollection
 
 def load_model_from_config(config):
     datamodule = SyntheticDatasetCollection()
-    model = CT(args=config, dataset_collection=datamodule)
+    model = CT(
+        args=config,
+        dataset_collection=datamodule,
+        autoregressive=config.model.autoregressive  # ✅ this line is the fix
+    )
     return model, datamodule
+
