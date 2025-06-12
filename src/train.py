fdbc003204267cb6428bd136d40a96976ea05619
EleazarMartin <135169782+EleazarMartin@users.noreply.github.com>
2025-06-12 17:11:55 -0400

    Update train.py

--- src/train.py
+++ src/train.py
@@ -2,6 +2,6 @@ from src.models.ct import CT
 from src.data.cancer_sim.dataset import SyntheticDatasetCollection
 
 def load_model_from_config(config):
-    datamodule = SyntheticDatasetCollection(config['dataset'])
+    datamodule = SyntheticDatasetCollection()
     model = CT(args=config, dataset_collection=datamodule)
     return model, datamodule
